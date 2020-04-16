354521bbfc0b9f8891b8f26060e165c6c9585477
Brandon Nelson <theredraver@gmail.com>
2020-04-15 23:42:18 -0700

    update syntax

--- src/tests/integration/test_warcraft_client.py
+++ src/tests/integration/test_warcraft_client.py
@@ -5,4 +5,4 @@ from clients.warcraft_client import WarcraftClient
 def test_get_achievement_category_index(snapshot):
     client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
     response = client.get_achievement_category_index()
-    snapshot.assert_match(response)
+    assert response == snapshot
--- src/tests/spec/test_spec_differ.py
+++ src/tests/spec/test_spec_differ.py
@@ -5,4 +5,4 @@ def test_api_spec_diff(snapshot):
     response = requests.get(
         "https://develop.battle.net/api/pages/content/documentation/world-of-warcraft/game-data-apis.json"
     ).json()
-    snapshot.assert_match(response)
+    assert response == snapshot
