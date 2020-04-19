b332a0927a51b454fddaca7397bc7c85cd15a6bc
Brandon Nelson <theredraver@gmail.com>
2020-04-18 18:07:18 -0700

    add more integration tests

--- src/tests/integration/test_warcraft_client.py
+++ src/tests/integration/test_warcraft_client.py
@@ -216,3 +216,81 @@ def test_get_mount(snapshot):
     client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
     response = client.get_mount(6)
     assert response == snapshot
+
+
+def test_get_mythic_keystone_affixes_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_affixes_index()
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_affix(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_affix(1)
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_affix_media(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_affix_media(1)
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_dungeons_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_dungeons_index()
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_dungeon(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_dungeon(353)
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_index()
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_periods_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_periods_index()
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_period(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_period(641)
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_seasons_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_seasons_index()
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_season(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_season(1)
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_leaderboards_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_leaderboards_index(11)
+    assert response == snapshot
+
+
+def test_get_mythic_keystone_leaderboard(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_keystone_leaderboard(11, 197, 641)
+    assert response == snapshot
+
+
+def test_get_mythic_raid_leaderboard(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_mythic_raid_leaderboard("uldir", "alliance")
+    assert response == snapshot
