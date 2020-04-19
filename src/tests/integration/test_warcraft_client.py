372661ce374c96f7b64dd688632553af3b653fb4
Brandon Nelson <theredraver@gmail.com>
2020-04-18 17:47:16 -0700

    add more integration tests

--- src/tests/integration/test_warcraft_client.py
+++ src/tests/integration/test_warcraft_client.py
@@ -102,3 +102,63 @@ def test_get_creature_family_media(snapshot):
     client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
     response = client.get_creature_family_media(1)
     assert response == snapshot
+
+
+def test_get_guild_crest_components_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_guild_crest_components_index()
+    assert response == snapshot
+
+
+def test_get_guild_crest_border_media(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_guild_crest_border_media(0)
+    assert response == snapshot
+
+
+def test_get_guild_crest_emblem_media(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_guild_crest_emblem_media(0)
+    assert response == snapshot
+
+
+def test_get_item_classes_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item_classes_index()
+    assert response == snapshot
+
+
+def test_get_item_class(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item_class(0)
+    assert response == snapshot
+
+
+def test_get_item_sets_index(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item_sets_index()
+    assert response == snapshot
+
+
+def test_get_item_set(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item_set(1)
+    assert response == snapshot
+
+
+def test_get_item_subclass(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item_subclass(0, 0)
+    assert response == snapshot
+
+
+def test_get_item(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item(19019)
+    assert response == snapshot
+
+
+def test_get_item_media(snapshot):
+    client = WarcraftClient(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
+    response = client.get_item_media(19019)
+    assert response == snapshot
